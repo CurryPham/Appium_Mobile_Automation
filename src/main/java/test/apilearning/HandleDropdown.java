b62edeaf8c358c85864de72844fb09e0a321704c
Curry Pham <khoapd2000@gmail.com>
2023-05-13 22:47:08 +0700

    Handle Dropdown menu on Mobile

---
+++ src/main/java/test/apilearning/HandleDropdown.java
@@ -0,0 +1,25 @@
+package test.apilearning;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import utils.AppiumDriverEx;
+
+public class HandleDropdown {
+    public static void main(String[] args) {
+
+        // Set up the session
+        AppiumDriver<MobileElement> appiumDriver = AppiumDriverEx.getAppiumDriver();
+
+        // Go to Forms
+        MobileElement formLabel = appiumDriver.findElementByAccessibilityId("Forms");
+        formLabel.click();
+
+        // Click on the Dropdown element
+        MobileElement dropdownElement = appiumDriver.findElementByAccessibilityId("select-Dropdown");
+        dropdownElement.click();
+
+        // Click on the Dropdown Option
+        MobileElement dropdownOption = appiumDriver.findElementByXPath("//*[@text='webdriver.io is awesome']");
+        dropdownOption.click();
+    }
+}
