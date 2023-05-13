cba1dfd032c286a840081039cbe631527f15d941
Curry Pham <khoapd2000@gmail.com>
2023-05-13 20:47:15 +0700

    first commit

---
+++ src/main/java/test/LaunchApp.java
@@ -0,0 +1,33 @@
+package test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class LaunchApp {
+    public static void main(String[] args) {
+
+        try {
+            // Driver instance
+            AppiumDriver<MobileElement> appiumDriver;
+
+            // Set DesiredCapabilities to send to Appium server
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+            desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+            desiredCapabilities.setCapability(MobileCapabilityType.APP, "D:\\App\\Android.apk");
+
+
+            // Set up the Appium server URL to connect to
+            URL appiumServerURL = new URL("http://localhost:4723/wd/hub");
+            appiumDriver = new AppiumDriver<>(appiumServerURL, desiredCapabilities);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
