1f5e0dfbb7a292a756aabcb5d4bfbbf6a5225c8f
Curry Pham <khoapd2000@gmail.com>
2023-05-14 08:11:12 +0700

    Swipe Mobile Screen vertically

---
+++ src/main/java/test/apilearning/SwipeVertically.java
@@ -0,0 +1,59 @@
+package test.apilearning;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.AppiumDriverEx;
+
+import java.time.Duration;
+
+public class SwipeVertically {
+    public static void main(String[] args) {
+        // Set up the session
+        AppiumDriver<MobileElement> appiumDriver = AppiumDriverEx.getAppiumDriver();
+
+        // Go to Forms
+        appiumDriver.findElementByAccessibilityId("Forms").click();
+
+        // Make sure I'm on the targer screen
+        WebDriverWait wait = new WebDriverWait(appiumDriver, 30L);
+        wait.until(ExpectedConditions.visibilityOf(appiumDriver.findElementByAccessibilityId("switch-text")));
+
+        // Get the mobile screen sizes
+        Dimension windowSize = appiumDriver.manage().window().getSize();
+        int screenHeight = windowSize.getHeight();
+        int screenWidth = windowSize.getWidth();
+
+        // Init start points and end points to touch and release
+        int xStartPoint = 50 * screenHeight / 100;
+        int xEndPoint = xStartPoint;
+        int yStartPoint = 90 * screenWidth / 100;
+        int yEndPoint = 10 * screenWidth / 100;
+
+        // Perform the touch action
+        PointOption startPoint = new PointOption().withCoordinates(xStartPoint, yStartPoint);
+        PointOption endPoint = new PointOption().withCoordinates(xEndPoint, yEndPoint);
+
+        // Scroll down
+        TouchAction touchAction = new TouchAction(appiumDriver);
+        touchAction.press(startPoint)
+                .waitAction(new WaitOptions().withDuration(Duration.ofSeconds(1)))
+                .moveTo(endPoint)
+                .release()
+                .perform();
+
+        // Scroll up
+        touchAction.press(endPoint)
+                .waitAction(new WaitOptions().withDuration(Duration.ofSeconds(1)))
+                .moveTo(startPoint)
+                .release()
+                .perform();
+
+        appiumDriver.quit();
+    }
+}
